ae6ae94b588a72d90dbb8f101dd013b8e447e943
pabloleo-96 <Pablo.Leo@alu.uclm.es>
2018-06-06 11:39:11 +0000

    BatchGenerator and create model

---
+++ create_model.py
@@ -0,0 +1,33 @@
+from keras.applications import vgg16
+from keras.layers import Dropout, Dense
+from keras import Model
+
+def create_VGG():
+    
+    # set the image input size
+    image_size = 224
+    
+    # load the pre-trained model
+    vgg_16 = vgg16.VGG16(include_top = True, weights='imagenet', input_shape = (image_size, image_size, 3))
+
+    # remove last layer
+    vgg_16.layers.pop()
+
+    # freeze all the layers
+    for layer in vgg_16.layers:
+        layer.trainable = False
+
+    # get the output of the model
+    x = vgg_16.layers[-1].output
+
+    # add last FC layer
+    x = Dropout(0.6)(x)
+    x = Dense(10, kernel_initializer = 'he_uniform', activation = 'softmax')(x)
+
+    # create the new model
+    vgg_16 = Model(vgg_16.input, x)
+
+    # show summary
+    vgg_16.summary()
+    
+    return vgg_16
